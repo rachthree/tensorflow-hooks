e601408d04c9bcc6b89614f058d598b4eea6b7b9
Craig Chan <46288912+rachthree@users.noreply.github.com>
2024-03-27 21:38:50 -0400

    Move source code to src, update pyproject.toml

--- src/tf_hooks/__init__.py
+++ src/tf_hooks/__init__.py
@@ -1,3 +1,7 @@
+from importlib import metadata
+
 from .register import register_forward_hook, register_forward_pre_hook
+
+__version__ = metadata.version("tensorflow-hooks")
 
 __all__ = ["register_forward_hook", "register_forward_pre_hook"]
